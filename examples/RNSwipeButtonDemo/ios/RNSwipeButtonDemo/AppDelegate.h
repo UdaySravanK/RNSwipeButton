01ee761a3998b768ae504aad6107070b68b5fa62
UdaySravanK <UdaySravanKumar.Kamineni@target.com>
2019-08-31 15:54:55 -0500

    updated ios pods

--- examples/RNSwipeButtonDemo/ios/RNSwipeButtonDemo/AppDelegate.h
+++ examples/RNSwipeButtonDemo/ios/RNSwipeButtonDemo/AppDelegate.h
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIUserActivity.h>
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
