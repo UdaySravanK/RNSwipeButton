a67264a2a4bca2c8eae8aaf6672499f31c0d90c7
UdaySravanK <udaysravankumar.kamineni@target.com>
2020-08-15 16:45:58 -0500

    Updated demo app with react-native 0.63.2 rn-swipe-button@1.2.8 has issue AnimatedValue: Attempting to set value to undefined. rn-swipe-button@1.2.9 fixes it.

--- examples/RNSwipeButtonDemo/App.js
+++ examples/RNSwipeButtonDemo/App.js
@@ -3,11 +3,12 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import thumbIcon from './assets/thumbIcon.png';
@@ -16,81 +17,76 @@ import styles from './styles';
 
 import SwipeButton from 'rn-swipe-button';
 /**
- *
  * Follow below steps to test live changes of rn-swipe-button.
- *
  * 1. Copy 'src' folder from RNSwipeButton to 'RNSwipeButton/examples/RNSwipeButtonDemo' folder
  * 2. Comment above SwipeButton import and uncomment below one
  *
  * Note: NPM linking has some issue react-native/HAUL build tools.
  */
-
 // import SwipeButton from './src/components/SwipeButton';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'swipe status appears here',
-    };
-  }
-  showToastMessage = message => this.setState({message});
-  renderSubHeading = heading => (
+
+const App: () => React$Node = () => {
+  const defaultStatusMessage = 'swipe status appears here';
+  const [swipeStatusMessage, setSwipeStatusMessage] = useState(
+    defaultStatusMessage,
+  );
+
+  const TwitterIcon = () => <Icon name="twitter" color="#3b5998" size={30} />;
+  const FacebookIcon = () => <Icon name="facebook" color="#3b5998" size={30} />;
+
+  setInterval(() => setSwipeStatusMessage(defaultStatusMessage), 5000);
+  const updateSwipeStatusMessage = (message) => setSwipeStatusMessage(message);
+  const renderSubHeading = (heading) => (
     <Text style={styles.subHeading}>{heading}</Text>
   );
-  renderSwipeStatus = () => (
-    <Text style={styles.swipeStatus}>{this.state.message}</Text>
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.title}>React Native Swipe Button</Text>
+          <Text style={styles.swipeStatus}>{swipeStatusMessage}</Text>
+          {renderSubHeading('Disabled')}
+          <SwipeButton thumbIconImageSource={arrowRight} disabled />
+          {renderSubHeading('Swipe status callbacks')}
+          <SwipeButton
+            thumbIconImageSource={arrowRight}
+            onSwipeStart={() => updateSwipeStatusMessage('Swipe started!')}
+            onSwipeFail={() => updateSwipeStatusMessage('Incomplete swipe!')}
+            onSwipeSuccess={() =>
+              updateSwipeStatusMessage('Submitted successfully!')
+            }
+          />
+          {renderSubHeading('Right to left swipe enabled')}
+          <SwipeButton
+            enableRightToLeftSwipe
+            thumbIconBackgroundColor="#FFFFFF"
+            thumbIconComponent={FacebookIcon}
+            title="Slide to unlock"
+            onSwipeSuccess={() => updateSwipeStatusMessage('Slide success!')}
+          />
+          {renderSubHeading('Set a component as thumb icon')}
+          <SwipeButton
+            thumbIconBackgroundColor="#FFFFFF"
+            thumbIconComponent={TwitterIcon}
+            title="Slide to unlock"
+            railStyles={{
+              backgroundColor: '#44000088',
+              borderColor: '#880000FF',
+            }}
+          />
+          {renderSubHeading('Set .png image as thumb icon')}
+          <SwipeButton thumbIconImageSource={thumbIcon} />
+          {renderSubHeading('Set height')}
+          <SwipeButton height={25} />
+          {renderSubHeading('Set height and width')}
+          <SwipeButton height={35} width={150} title="Swipe" />
+        </View>
+      </SafeAreaView>
+    </>
   );
-  render() {
-    const TwitterIcon = () => <Icon name="twitter" color="#3b5998" size={30} />;
-    const FacebookIcon = () => (
-      <Icon name="facebook" color="#3b5998" size={30} />
-    );
-
-    setInterval(
-      () => this.setState({message: 'swipe status appears here'}),
-      5000,
-    );
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>React Native Swipe Button</Text>
-        {this.renderSwipeStatus()}
-        {this.renderSubHeading('Disabled')}
-        <SwipeButton thumbIconImageSource={arrowRight} disabled />
-        {this.renderSubHeading('Swipe status callbacks')}
-        <SwipeButton
-          thumbIconImageSource={arrowRight}
-          onSwipeStart={() => this.showToastMessage('Swipe started!')}
-          onSwipeFail={() => this.showToastMessage('Incomplete swipe!')}
-          onSwipeSuccess={() =>
-            this.showToastMessage('Submitted successfully!')
-          }
-        />
-        {this.renderSubHeading('Right to left swipe enabled')}
-        <SwipeButton
-          enableRightToLeftSwipe
-          thumbIconBackgroundColor="#FFFFFF"
-          thumbIconComponent={FacebookIcon}
-          title="Slide to unlock"
-          onSwipeSuccess={() => this.showToastMessage('Slide success!')}
-        />
-        {this.renderSubHeading('Set a component as thumb icon')}
-        <SwipeButton
-          thumbIconBackgroundColor="#FFFFFF"
-          thumbIconComponent={TwitterIcon}
-          title="Slide to unlock"
-          railStyles={{backgroundColor:'#44000088', borderColor: '#880000FF'}}
-        />
-        {this.renderSubHeading('Set .png image as thumb icon')}
-        <SwipeButton thumbIconImageSource={thumbIcon} />
-        {this.renderSubHeading('Set height')}
-        <SwipeButton height={25} />
-        {this.renderSubHeading('Set height and width')}
-        <SwipeButton height={35} width={150} title="Swipe" />
-      </View>
-    );
-  }
-}
+};
 
 export default App;
