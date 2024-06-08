b035e5abfce14a3d581792fd48abc9eceb7d86da
Uday Sravan K <usk.kamineni@gmail.com>
2024-06-08 18:11:40 -0400

    Replace legacy eslintrc config with new eslint.config.mjs (#129)
    * Removed all the legacy lint config to add the latest
    
    * Added the ESLint with new config

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  "extends": ["plugin:prettier/recommended"],
-  "rules": {
-    "prettier/prettier": "error"
-  }
-};
--- src/components/SwipeThumb/styles.js
+++ src/components/SwipeThumb/styles.js
@@ -1,30 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const borderWidth = 3;
 const margin = 1;
 const maxContainerHeight = 100;
 const Styles = StyleSheet.create({
   container: {
-    alignItems: 'flex-end',
-    alignSelf: 'flex-start',
+    alignItems: "flex-end",
+    alignSelf: "flex-start",
     borderRadius: maxContainerHeight / 2,
     borderRightWidth: 0,
     borderWidth,
     margin,
   },
   containerRTL: {
-    alignItems: 'flex-start',
-    alignSelf: 'flex-end',
+    alignItems: "flex-start",
+    alignSelf: "flex-end",
     borderRadius: maxContainerHeight / 2,
     borderLeftWidth: 0,
     borderWidth,
     margin,
   },
   icon: {
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: maxContainerHeight / 2,
     borderWidth: 2,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginVertical: -borderWidth,
   },
 });
